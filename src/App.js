b2be895312e0af4b4c4c8df603b54b59bd5dbd89
Neha <nehatualea@gmail.com>
2023-04-25 02:58:33 +0530

    root component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ProfileCard from './ProfileCard';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ProfileCard  image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEm9o7VcwIp4boMGk8Fqjql_gwciUQzzaJ-Q&usqp=CAU" designation="UI / UX  DESIGNER"  name="Aryan Shirani" jobDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque iaculis leo ultricies accumsan. Fusce consequat ante vitae"  see-more-btn="See more..."/>
+      <ProfileCard  image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1N5foCzekwyzh27oRiWnpX1SCxljj1DCgMw&usqp=CAU" designation="Freelancer / Developer "  name="Michele Nile" jobDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque iaculis leo ultricies accumsan. Fusce consequat ante vitae"  see-more-btn="See more..."/>
+      <ProfileCard  image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTknTV4TOO96F4g9GQmy712gdI4W-1dAhitIw&usqp=CAU" designation= "Software Engineer"  name="Carli Mini" jobDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque iaculis leo ultricies accumsan. Fusce consequat ante vitae"  see-more-btn="See more..."/>
       </header>
     </div>
   );
