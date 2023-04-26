0413a350b85fc09f9102b2c7d45cd5a120c69a3a
Neha <nehatualea@gmail.com>
2023-04-26 05:47:36 +0530

    List and keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card from './Card';
 
 function App() {
+  
+  const productDetails = [
+    {
+      productId : '1',
+      productPic : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS0iXbSWW2riIzv5T8rKuh8seHCtj_FspBxi7l5s2u0-fgUlLNU",
+      productName : "SHOES",
+      Brand : "Adidas",
+      productDescription : "Hey Dude Men's Wally Linen Iron Size 11 | Mens Shoes | ",
+      Price : "INR 4301",
+      Rating : "⭐⭐⭐⭐ - Rating-4"
+    },
+    {
+      productId : '2',
+      productPic : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTessE3KZD9eAQWQ9x9o8dCMfaVWw63_bEeEBpc2yUKhV-wbPDZ",
+      productName : "WATCH",
+      Brand : "Curion",
+      productDescription : "Hey Dude Men's Wally Linen Iron Size 11 | Mens Shoes | ",
+      Price : "INR 1091",
+      Rating : "⭐⭐⭐⭐ - Rating-4"
+    },
+    {
+      productId : '3',
+      productPic : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqTwxNF6kokdgOxUr4qDdxuSZBqpi6qQQT6UkwoZu1-_qSEGG7",
+      productName : "T-SHIRT",
+      Brand : "Adidas",
+      productDescription : "Hey Dude Men's T-Shirt Size 11 | Mens Shirt | ",
+      Price : "INR 2599",
+      Rating : "⭐⭐⭐⭐ - Rating-4"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-card">
+        {productDetails.map(productDetail => (
+          <Card key={productDetail.productId} productDetails={productDetail} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,30 @@
+import "./Card.css"
+export default function Card({productDetails}){
+
+    const { 
+productId ,
+    productPic ,
+    productName ,
+    Brand ,
+    productDescription,
+    Price ,
+    Rating 
+
+    }=productDetails;
+    
+    function handleOnClick(){
+        alert(" Product of is  " + productId + ", which is " + productName +" of brand "  + Brand + " has of rupees " + Price + " is added in the cart , please proceed to pay !!! ")
+    }
+    return(
+        <div className="card">
+        <img src= {productPic} alt="" height="230px" width="200px"/>
+        <h3 className="grey pname">{productName}</h3>
+        <p className="description grey">{productDescription}</p>
+        <h3 className="yellow">{Price}</h3>
+        <p>{Rating}</p>
+        <button className="btn-add" onClick={handleOnClick}>Add to Cart</button>
+        </div>
+    );
+}
+
+
