d9da8e2ae26b8801739cf9f0679876c11372ec41
Neha <nehatualea@gmail.com>
2023-05-25 02:55:23 +0530

    Redux to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AddTodo from './components/AddTodo';
+import { TodoList } from './components/TodoList'
+import TotalCompleteItems from './components/TotalCompleteItems';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add to do</h1>
+      <AddTodo />
+      <TodoList />
+      <TotalCompleteItems />
     </div>
   );
 }
